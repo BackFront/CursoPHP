4977c481ca2abb14c9a6a7d79fbbacb1fcd1006f
Douglas Alves <alves.douglaz@gmail.com>
2016-06-30 12:48:31 -0300

    [FIX] - separador de diretorios dinamico no autoload

--- autoload.php
+++ autoload.php
@@ -1,10 +1,17 @@
 <?php
-function custon_autoload($Class) {
-    $dirName = __DIR__.'/src';
-    if (file_exists("{$dirName}/{$Class}.php")):
-        require_once("{$dirName}/{$Class}.php");
+
+function custon_autoload($Class)
+{
+    $dirName = __DIR__ . '/src';
+    $dir = "{$dirName}/{$Class}.php";
+    $dir = str_replace('\\', DIRECTORY_SEPARATOR, $dir);
+
+    if(file_exists($dir)):
+        require_once($dir);
     else:
+        echo "{$dirName}/{$Class}.php <br>";
         die("Erro ao incluir a classe: #{$Class} - no diretorio: {$dirName}<hr />");
     endif;
 }
+
 spl_autoload_register("custon_autoload");
